f851a7a58d3d83b77b95c3dddf194ca3374615b0
tejakumar07 <tejakumarwarrior@gmail.com>
2024-11-21 14:00:32 +0530

    first commit

--- week_1/pattern/pattern16.py
+++ week_1/pattern/pattern16.py
@@ -9,8 +9,22 @@
 
 """
 
-num = 65
-n=6
+n = int(input("What's N: "))
 
-jkbjnbkj,
-    
+for i in range(0,n):
+    #This is for Spaces
+    for j in range(0,n-i+1):
+        print(" ",end="")
+    #This is for alphabets
+    alpha = 65
+    for j in range(0,2*i+1):
+        breakpoint = i
+        print(chr(alpha),end="")
+        if j < breakpoint:
+            alpha+=1
+        else:
+            alpha-=1
+    #This for the Spaces
+    for j in range(0,n-i+1):
+        print(" ",end="")
+    print("")
