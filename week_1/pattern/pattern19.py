a921bc9eb7a6ef04bce8e81cb58931879246bbb0
tejakumar07 <tejakumarwarrior@gmail.com>
2024-11-21 14:09:34 +0530

    first commit

--- week_1/pattern/pattern19.py
+++ week_1/pattern/pattern19.py
@@ -1,2 +1,36 @@
+# This is another Pattern
+'''
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+'''
 
 
+n = int(input("what's N: "))
+
+# This is for the First Symmetry
+for i in range(1, n+1):
+    for j in range(0, i):
+        print("*", end="")
+    for j in range(0, 2 * (n - i)):
+        print(" ", end="")
+    for j in range(0, i):
+        print("*", end="")
+    print("")
+
+# This is for the Secondary Symmetry
+
+for i in range(n,0,-1):
+    for j in range(0,i):
+        print("*",end="")
+    for j in range(0,2*(n-i)):
+        print(" ",end="")
+    for j in range(0,i):
+        print("*",end="")
+    print("")
