304a03f707cf06828c1a97460030736840aa90dc
tejakumar07 <tejakumarwarrior@gmail.com>
2024-11-21 11:12:13 +0530

    first commit

--- week_1/pattern/pattern16.py
+++ week_1/pattern/pattern16.py
@@ -12,12 +12,5 @@
 num = 65
 n=6
 
-for i in range(1,n+1):
-    for j in range(n,i,-1):
-        print("",end="")
-    for j in range(1,2*i-1):
-        print("*",end="")
-    for j in range(n,1,-1):
-        print("",end="")
-    print("")
+jkbjnbkj,
     
