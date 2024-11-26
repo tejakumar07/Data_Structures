d80e3cca51205927c84c4749d2b5017ea9c9af3a
TEJA KUMAR VASAMSETTI <107473361+tejakumar07@users.noreply.github.com>
2024-11-26 10:00:04 +0000

    first commit

---
+++ week_1/basic_maths/euclideanalgorithm.py
@@ -0,0 +1,21 @@
+# This code is about GCD but solving using Euclidean Algorithm
+# Euclidean Algorithm Proof for GCD is : gcd(a,b) = gcd(a-b,b) where a > b
+
+# Function to calculate GCD using Euclidean algorithm
+def gcd(a, b):
+    # Continue looping until b becomes 0
+    while b != 0:
+        # Update a to be the value of b, and b to be the remainder of a divided by b
+        a, b = b, a % b
+    # Once b is 0, a will contain the GCD
+    return a
+
+# Take user input for the two numbers
+a = int(input())  # First number
+b = int(input())  # Second number
+
+# Call the gcd function and store the result in the variable 'hcf'
+hcf = gcd(a, b)
+
+# Output the GCD (HCF) value
+print(hcf)
