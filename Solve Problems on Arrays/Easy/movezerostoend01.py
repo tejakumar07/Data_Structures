2efed60d6e67ba1178b194fa966459bf6896dc19
tejakumar07 <tejakumarwarrior@gmail.com>
2025-05-31 17:16:34 +0530

    bug fixed

--- Solve Problems on Arrays/Easy/movezerostoend01.py
+++ Solve Problems on Arrays/Easy/movezerostoend01.py
@@ -1,11 +1,14 @@
 # Optimal Solution
-def MoveZerosToEnd(arr):
-    i = 0
-    j = 1
-    while (j < len(arr)):
+def pushZerosAtEnd(arr):
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j+=1
+    return arr
         
 
 
 if __name__ == "__main__":
     arr = [1,2,0,0,3,0,0,4,0,0,5,0,0,6,0,7,0,0,0,8]
-    print(MoveZerosTOEnd(arr))
+    print(pushZerosAtEnd(arr))
