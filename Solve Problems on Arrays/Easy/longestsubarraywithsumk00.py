44c756eddd367231573bf3a62dcf9b38a481aa94
TEJA KUMAR VASAMSETTI <107473361+tejakumar07@users.noreply.github.com>
2025-07-15 16:18:28 +0530

    Update longestsubarraywithsumk00.py

--- Solve Problems on Arrays/Easy/longestsubarraywithsumk00.py
+++ Solve Problems on Arrays/Easy/longestsubarraywithsumk00.py
@@ -1,15 +1,17 @@
 # Brute Force Approach
 def LongestSubArray(arr, k):
-    length = 0
-    for i in range(0, len(arr)):
-        for j in range(i, len(arr)):
-            total_sum = 0
-            for l in range(i, j+1):
-                total_sum += arr[l]
-            if total_sum == k:
-                length = max(length, j-i+1)
-    return length
+    n = len(nums)
+    maxi = 1
 
+    for i in range(n):
+        for j in range(i+1, n):
+            current_sum = 0
+            for k in range(i, j + 1):
+                current_sum += nums[k]
+            
+            if current_sum == target:
+                maxi = max(maxi, j - i + 1)
+    return maxi
 
 if __name__ == "__main__":
     arr = [1,2,3,1,1,1,1,4,2,3]
