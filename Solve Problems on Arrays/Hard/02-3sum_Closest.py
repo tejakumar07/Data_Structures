6d4a6fab2f996dc384ee842211a8cd3217a103b8
TEJA KUMAR VASAMSETTI <107473361+tejakumar07@users.noreply.github.com>
2025-07-19 21:05:04 +0530

    Update 02-3sum_Closest.py

--- Solve Problems on Arrays/Hard/02-3sum_Closest.py
+++ Solve Problems on Arrays/Hard/02-3sum_Closest.py
@@ -1,28 +1,23 @@
 # Optimal Solution
 
 def three_sum_closest(nums, target):
-    n = len(nums)
-    if n < 3:
-        return nums
     nums.sort()
-
+    n = len(nums)
     closest = nums[0] + nums[1] + nums[2]
-
+    
     for i in range(n - 2):
         left, right = i + 1, n - 1
-        
         while left < right:
-            current_sum = nums[i] + nums[left] + nums[right]
+            _sum = nums[i] + nums[left] + nums[right]
+            
+            if (abs(target - _sum) < abs(target - closest)) or \
+               (abs(target - _sum) == abs(target - closest) and _sum < closest):
+                closest = _sum
 
-            if abs(target -current_sum) < abs(target - closest):
-                closest = current_sum
-            
-            if current_sum < target:
+            if _sum < target:
                 left += 1
-            
-            elif current_sum > target:
+            elif _sum > target:
                 right -= 1
-            
             else:
                 return target
     return closest
