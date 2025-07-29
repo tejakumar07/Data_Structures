484ead8377dc750738361c82e0a29210628e000b
tejakumar07 <tejakumarwarrior@gmail.com>
2025-07-29 14:00:16 +0530

    Add implementation for largest subarray sum minimized problem

---
+++ Binary Search/BS on Answers/Split_Array__Largest_Sum/01-main.py
@@ -0,0 +1,27 @@
+def countPartitions(a, maxSum):
+    n = len(a)  # size of array
+    partitions = 1
+    subarraySum = 0
+    for i in range(n):
+        if subarraySum + a[i] <= maxSum:
+            # insert element to current subarray
+            subarraySum += a[i]
+        else:
+            # insert element to next subarray
+            partitions += 1
+            subarraySum = a[i]
+    return partitions
+
+def largestSubarraySumMinimized(a, k):
+    low = max(a)
+    high = sum(a)
+
+    for maxSum in range(low, high+1):
+        if countPartitions(a, maxSum) == k:
+            return maxSum
+    return low
+
+a = [10, 20, 30, 40]
+k = 2
+ans = largestSubarraySumMinimized(a, k)
+print("The answer is:", ans)
