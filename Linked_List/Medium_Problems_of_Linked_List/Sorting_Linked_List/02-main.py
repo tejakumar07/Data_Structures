380539fa0e6d00a5512c379b07f6e85ed6ef65e2
tejakumar07 <tejakumarwarrior@gmail.com>
2025-09-16 09:49:43 +0530

    Refactor linked list methods for clarity and consistency

--- Linked_List/Medium_Problems_of_Linked_List/Sorting_Linked_List/02-main.py
+++ Linked_List/Medium_Problems_of_Linked_List/Sorting_Linked_List/02-main.py
@@ -1,13 +1,12 @@
-# Optimal Solution
-
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next=None):
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def display(self, head):
-        if head is None or head.next is None:
+        if head is None:
             return "Linked List is Empty"
 
         itr = head
@@ -16,24 +15,23 @@ class LinkedList:
         while itr:
             llstr += str(itr.data) + "--->" if itr.next else str(itr.data)
             itr = itr.next
-        
+
         return llstr
 
-    def finding_middle(self, head):
+    def find_middle(self, head):
         if head is None or head.next is None:
             return head
-        
+
         slow = head
         fast = head.next
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
+
         return slow
 
-    
-    def merge_linked_list(self, list1, list2):
+    def merge_sorted_linked_list(self, list1, list2):
         dummy_node = Node(-1)
         temp = dummy_node
 
@@ -41,46 +39,48 @@ class LinkedList:
             if list1.data <= list2.data:
                 temp.next = list1
                 list1 = list1.next
-            
             else:
                 temp.next = list2
                 list2 = list2.next
-            
             temp = temp.next
-        
+
         while list1 is not None:
             temp.next = list1
+            list1 = list1.next
             temp = temp.next
-        
+
         while list2 is not None:
             temp.next = list2
+            list2 = list2.next
             temp = temp.next
-        
+
         return dummy_node.next
 
-    def sort_linked_list(self, head):
+    def merge_linked_list(self, head):
         if head is None or head.next is None:
             return head
-        
-        middle = self.finding_middle(head)
 
+        middle = self.find_middle(head)
         left = head
         right = middle.next
         middle.next = None
 
+        left = self.merge_linked_list(left)
+        right = self.merge_linked_list(right)
 
-        left = self.sort_linked_list(left)
-        right = self.sort_linked_list(right)
+        return self.merge_sorted_linked_list(left, right)
 
-        return self.merge_linked_list(left, right)
 
 if __name__ == "__main__":
     ll = LinkedList()
-    head = Node(13)
-    head.next = Node(53)
-    head.next.next = Node(2)
-    head.next.next.next = Node(17)
-    head.next.next.next.next = Node(4)
-
+    head = Node(14)
+    head.next = Node(2)
+    head.next.next = Node(56)
+    head.next.next.next = Node(12)
+    head.next.next.next.next = Node(1)
+    head.next.next.next.next.next = Node(32)
+    print("Orginal Linked List")
     print(ll.display(head))
-    print(ll.merge_linked_list(head))
+    print("After Sorting Linked List")
+    head = ll.merge_linked_list(head)
+    print(ll.display(head))
